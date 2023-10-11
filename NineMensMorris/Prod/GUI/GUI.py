348e18c223b3983b9629070d6f3bf4083f117872
DanielRider <98618136+DanielRider@users.noreply.github.com>
2023-10-11 16:31:38 +0000

    added GUI class

--- NineMensMorris/Prod/GUI/GUI.py
+++ NineMensMorris/Prod/GUI/GUI.py
@@ -1,8 +1,32 @@
-import tkinter
-import Board
+import sys
+sys.path.append('NineMensMorris/Prod/NineMenMorrisInterface')  
+import tkinter as tk
+from tkinter import messagebox
+from PieceLogic import Locations  
 
-class GUI(object):
-    """GUI"""
+
+class NineMansMorrisGUI:
+    def __init__(self):
+        # self.title('Nine Mans Morris')
+        # self.geometry('600x600')
+        self.locations = Locations()
+        self.buttons = {}
+        for i in range(24):
+            row = i // 4
+            col = i % 4
+            # self.buttons[i] = tk.Button(self, text=' ', width=10, height=3, command=lambda i=i: self.click(i))
+            # self.buttons[i].grid(row=row, column=col)
+
+    def click(self, position):
+        if self.locations.place_piece(position):
+            # self.buttons[position].config(text=str(self.locations.current_player))
+            print(f"Piece placed by {self.locations.current_player} at position {position}")
+        else:
+            print("Invalid move. Try again.")
+            # messagebox.showinfo('Invalid', 'Invalid move. Try again.')
 
 
+if __name__ == '__main__':
+    app = NineMansMorrisGUI()
+    # app.mainloop()
 
